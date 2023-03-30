46bfff88b06fef60d3b8f50a1e51f911a62d84f6
YuenEsc <yuen.escalante@gmail.com>
2023-03-30 11:36:17 -0600

    puppeteer example added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,31 @@ module.exports =
         borderWidth: ['responsive', 'first', 'last', 'odd', 'even'],
         borderColor: ['responsive', 'first', 'last', 'odd', 'even'],
       },
+      colors: {
+        textPrimary: 'rgb( var(--primary-text-color) )',
+        textSecondary: 'rgb( var(--secondary-text-color) )',
+        black: 'rgb( var(--black-color))',
+        gray : {
+          500: 'rgb( var(--gray-500-color) )',
+          50: 'rgb( var(--gray-50-color) )'
+        },
+        amber:{
+          500: 'rgb( var(--amber-500-color) )',
+          50: 'rgb( var(--amber-50-color) )'
+        },
+        orange:{
+          500: 'rgb( var(--orange-500-color) )',
+          50: 'rgb( var(--orange-50-color) )'
+        },
+        red:{
+          500: 'rgb( var(--red-500-color) )',
+          50: 'rgb( var(--red-50-color) )'
+        },
+        blue:{
+          50: 'rgb( var(--blue-50-color) )'
+        },
+        info: 'rgb( var(--info-color) ) '
+      },
       extend: {
 
       },
